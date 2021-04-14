4fc5f8e142a870d8ea4c15bc69771d089743ef6d
Mathieu Despont <mathieu.despont@ecodev.ch>
2021-04-14 23:22:14 +0200

    complexity controler

--- bis-generator.js
+++ bis-generator.js
@@ -30,6 +30,10 @@
 
   /////////// Initiatilsation  ////////////
 
+  var numberOfCode = d3.select("#"+htmlIdPrefix+"numberOfCode").node().value;
+
+
+
 //////// création de svg dynamique /////
   // Variable qui représente l'élément body sur lequel on se croche pour générer le contenu via d3.js
   var htmlBody = d3.select("#"+htmlIdPrefix+"zone");
@@ -64,11 +68,41 @@
     .attr("stroke", "orange")
     .attr("stroke-width", "20")
 
+
+    // lancement app
+
+    // au clic du bouton, regénère une marque de famille
+    d3.select("#"+htmlIdPrefix+"regenerateButton").on("click", function() {
+      hideAllSegment();
+
+      var numberOfCode = d3.select("#"+htmlIdPrefix+"numberOfCode").node().value;
+
+      var genome = genomeGenerator(numberOfCode);
+      // showMarkFromCode("vc"); // affiche le(s) segments qui correspondent à un code.
+      showMarkFromGenome(genome); // le génome est un tableau de code. ex: ["va","a3","o2","b5"]
+
+    });
+
+    // au changement de du slider regénère une markque de famille
+    d3.select("#"+htmlIdPrefix+"numberOfCode").on("change", function() {
+      hideAllSegment();
+
+      var numberOfCode = d3.select("#"+htmlIdPrefix+"numberOfCode").node().value;
+      //console.log(numberOfCode);
+
+      var genome = genomeGenerator(numberOfCode);
+      // showMarkFromCode("vc"); // affiche le(s) segments qui correspondent à un code.
+      showMarkFromGenome(genome); // le génome est un tableau de code. ex: ["va","a3","o2","b5"]
+
+    });
+
+
+    // initialisation de l'app au démarrage
     hideAllSegment(); // cache tous les segments car ils ont la classe "segment"
     //showAllSegment();
 
     //var genome = ["va","a3","o2","b5"];
-    var genome = genomeGenerator(5);
+    var genome = genomeGenerator(numberOfCode);
     // showMarkFromCode("vc"); // affiche le(s) segments qui correspondent à un code.
     showMarkFromGenome(genome); // le génome est un tableau de code. ex: ["va","a3","o2","b5"]
 
@@ -85,9 +119,9 @@
     // On rempli le tableau avec le nombre de code passés en paramètres
     for (var i = 0; i < numberOfCode; i++) {
 
-      // génère une des 5 base "bravo".
+      // génère une des 5 bases "bravo".
       var lettreBravo = "bravo";
-      var randomNumber = Math.floor(Math.random() * 4);  // génère un nombre aléatoire entre 0 et 4 => BRAVO
+      var randomNumber = Math.floor(Math.random() * 5);  // génère un nombre aléatoire entre 0 et 4 => BRAVO
       var randomBase = lettreBravo.slice(randomNumber, randomNumber+1);
 
       // génère un indice hexadécimal
