594dfea56789fe5c3f9510f8d5019dffd6cbb9bc
Mathieu Despont <mathieu.despont@ecodev.ch>
2021-04-21 19:36:39 +0200

    add php sendmail

---
+++ utile/send-message.php
@@ -0,0 +1,91 @@
+<?php
+/*******************************************************************************************
+ * Nom du fichier     : send-message.php
+ * Date		     	  : 21 avril 2021
+ * @author	     	  : Mathieu Despont
+ * But de ce fichier  : envoyer un e-mail avec une url qui permet de voir une marque de famille avec un genome connu
+ *******************************************************************************************
+ * Fichier appellé par ajax. On fourni 3 paramètres: pseudo, e-mail, et genome d'une marque de famille. ex: b3-r4-aa-v9-0f
+ *
+ *
+ */
+
+
+ob_start("ob_gzhandler");
+session_start();
+header('Content-Type: text/html; charset=UTF-8');
+date_default_timezone_set('Europe/Zurich');
+
+if (isset($_GET['pseudo'])) {
+  $pseudo = $_GET['pseudo'];
+}else{
+  $pseudo = '';
+}
+
+if (isset($_GET['genome'])) {
+  $genome = $_GET['genome'];
+}else{
+  $genome = '';
+}
+
+if (isset($_GET['destinataire'])) {
+  $destinataire = $_GET['destinataire'];
+}else{
+  $destinataire = '';
+}
+
+sendMark();
+
+// todo tester le pattern du mail.
+// todo: on veut l'url.. https://ecodev.ch/bis/marque.html?genome=a1-r3-b4&pseudo=Momo  urldecode ... etc..
+
+
+function sendMark(){
+
+  // repris de https://www.php.net/manual/fr/function.mail.php
+
+  $to      = 'blanc@martouf.ch';
+  $subject = 'Marque de famille';
+  $message = '
+     <html>
+      <head>
+       <title>Marque de famille</title>
+      </head>
+      <body>
+       <p>Bonjour, \r\n Voici la marque de famille de '+$pseudo+'.</p>
+       <p>Cliquez sur le lien ci-dessous pour voir la marque.</p>
+       <p><a href="https://ecodev.ch/bis/marque.html?genome=a1-r3-b4">https://ecodev.ch/bis/marque.html?genome=a1-r3-b4</a></p>
+      </body>
+     </html>
+     ';
+
+
+  $headers = array(
+    'From' => 'mathieu@ecodev.ch',
+    'X-Mailer' => 'PHP/' . phpversion()
+  );
+  // Pour envoyer un mail HTML, l'en-tête Content-type doit être défini
+  $headers[] = 'MIME-Version: 1.0';
+  $headers[] = 'Content-type: text/html; charset=iso-8859-1';
+
+  mail($to, $subject, $message, $headers);
+
+}
+
+
+function testMail(){
+
+    echo "test de mail()";
+  $Name = "Toto"; //senders name
+  $email = "mathieu.despont@ecodev.ch"; //senders e-mail adress
+  $recipient = "mathieu@ecodev.ch"; //recipient
+  $mail_body = "Texte du mail..."; //mail body
+  $subject = "essai de mail php"; //subject
+  $header = "From: ". $Name . " <" . $email . ">\r\n"; //optional headerfields
+
+  echo "alors: ".$recipient.$subject.$mail_body.$header;
+
+  mail($recipient, $subject, $mail_body, $header); //mail command :)
+}
+
+echo "ok";
